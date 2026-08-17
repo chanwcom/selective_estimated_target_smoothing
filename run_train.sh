4313819a36f9ef955edee9b5831123560282cf48
Chanwoo Kim <chanwcom@gmail.com>
2026-08-17 10:18:07 +0900

    Update

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,11 @@
+PROFILE=libri_speech_full_960hr
+#PROFILE=libri_speech_clean_100hr
+#PROFILE=libri_light_10hr
+#PROFILE=libri_light_1hr
+
 python wav2vec2_finetuning_sets.py \
     --alpha=0.0  \
     --beta=0.0   \
     --vocab_size 32 \
-    --finetune_profile=libri_speech_clean_100hr
-#    --finetune_profile=libri_light_10hr
+    --finetune_profile=$PROFILE  \
+    --per_device_train_batch_size 20
