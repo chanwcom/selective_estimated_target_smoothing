5076f1e983cc417718f7da5f6c6a75f349da5a0e
Chanwoo Kim <chanwcom@gmail.com>
2026-08-17 16:16:30 +0900

    Update

--- set_config.sh
+++ set_config.sh
@@ -1,2 +1,7 @@
-export NCCL_P2P_DISABLE=1; export NCCL_IB_DISABLE=1; export CUDA_VISIBLE_DEVICES=0;
+CWK_SCRIPT_HOME=/mnt/data/home/chanwcom/local_repository/cognitive_workflow_kit_emnlp_2026/scripts
+DEVIDE_ID=0
+
+source $CWK_SCRIPT_HOME/setup_path.sh
+
+export NCCL_P2P_DISABLE=1; export NCCL_IB_DISABLE=1; export CUDA_VISIBLE_DEVICES=$DEVICE_ID;
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
